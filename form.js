42495634ec897a5e77869f277a9502daba84fca3
Siva <siva2580siva@gmail.com>
2025-07-05 01:01:24 -0700

    Updated form submission and alignments

---
+++ form.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("contactme");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent redirect
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+      method: "POST",
+      body: formData,
+    })
+      .then(response => {
+        if (response.ok) {
+          alert("Thanks for contacting!");
+          form.reset(); // Clear form fields
+        } else {
+          alert("Something went wrong. Please try again.");
+        }
+      })
+      .catch(error => {
+        alert("Error occurred!");
+        console.error(error);
+      });
+  });
